3c551b781c5c5ee81101f61613840a92d2e147f4
shinchan <akashkumar3432@gmail.com>
2021-03-21 23:03:48 +0530

    Updates

---
+++ precache-manifest.c1d3da4c727af7148fe7f8cf48ce3b5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a2939e5c43bdad6b4a416a74f641791",
+    "url": "/movie_search/index.html"
+  },
+  {
+    "revision": "a03352c8739a00de1c56",
+    "url": "/movie_search/static/css/main.cd29767b.chunk.css"
+  },
+  {
+    "revision": "3ca31e2eae15b78c1fd8",
+    "url": "/movie_search/static/js/2.5e3b199e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_search/static/js/2.5e3b199e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a03352c8739a00de1c56",
+    "url": "/movie_search/static/js/main.6b060dc6.chunk.js"
+  },
+  {
+    "revision": "debe7ff615263daab368",
+    "url": "/movie_search/static/js/runtime-main.50f2981e.js"
+  },
+  {
+    "revision": "01125eaa005dc8a3b434b307e28106cf",
+    "url": "/movie_search/static/media/pacman.01125eaa.svg"
+  }
+]);
